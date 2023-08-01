9641560401588bd069500074407900494d778df5
Luca <rossiluca711@gmail.com>
2023-08-01 16:38:53 +0200

    small fix (run_free)

--- run_free.py
+++ run_free.py
@@ -3,11 +3,11 @@ import torch
 import torch.backends.cudnn as cudnn
 from args import parse_args
 from modules.data import Data
-from modules.trainer_tfvaegan import TrainerTfvaegan
+from modules.trainer_free import TrainerFree
 from modules.similar_sample_finder import SimilarSampleFinder
 
 # parse arguments
-args = parse_args('TFVAEGAN')
+args = parse_args('FREE')
 # init seed and cuda
 if args.seed is None:
 	args.seed = random.randint(1, 10000)
@@ -22,15 +22,17 @@ cudnn.benchmark = True
 data = Data(dataset_name=args.dataset, dataroot=args.dataroot)
 # define similar sample finder
 similar_sample_finder = SimilarSampleFinder(data)
-# train the TF-VAEGAN
-tfvaegan = TrainerTfvaegan(data, args.dataset, similar_sample_finder, n_features=args.n_features, n_attributes=data.get_n_attributes(),
-			 			features_per_class=args.features_per_class, batch_size=args.batch_size,
-						hidden_size=args.hidden_size, n_epochs=args.n_epochs, n_classes=data.get_n_classes(),
-						n_critic_iters=args.n_critic_iters, n_loops=args.n_loops,
-						lr=args.lr, lr_feedback=args.lr_feedback, lr_decoder=args.lr_decoder, lr_cls=args.lr_cls,
-						beta1=args.beta1, freeze_dec=args.freeze_dec, vae_beta=args.vae_beta,
-						weight_gp=args.weight_gp, weight_critic=args.weight_critic, weight_generator=args.weight_generator,
-						weight_feed_train=args.weight_feed_train, weight_feed_eval=args.weight_feed_eval, weight_recons=args.weight_recons,
-						noise_size=data.get_n_attributes(), cond_size=args.cond_size, n_similar_classes=args.n_similar_classes,
-						agg_type=args.agg_type, pool_type=args.pool_type, save_every=args.save_every, device=device)
-tfvaegan.fit()
+# define center loss type depending on the type of dataset
+min_margin = args.dataset == 'AWA2'
+# train the FREE model
+free = TrainerFree(data, args.dataset, similar_sample_finder, n_features=args.n_features, n_attributes=data.get_n_attributes(),
+				features_per_class=args.features_per_class, batch_size=args.batch_size,
+				hidden_size=args.hidden_size, n_epochs=args.n_epochs, n_classes=data.get_n_classes(),
+				n_critic_iters=args.n_critic_iters, n_loops=args.n_loops,
+				lr=args.lr, lr_cls=args.lr_cls, beta1=args.beta1, freeze_dec=args.freeze_dec,
+				weight_gp=args.weight_gp, weight_critic=args.weight_critic, weight_generator=args.weight_generator,
+				center_margin=args.center_margin, weight_margin=args.weight_margin,
+				weight_center=args.weight_center, weight_recons=args.weight_recons,
+				noise_size=data.get_n_attributes(), cond_size=args.cond_size, n_similar_classes=args.n_similar_classes,
+				agg_type=args.agg_type, pool_type=args.pool_type, save_every=args.save_every, device=device)
+free.fit()
